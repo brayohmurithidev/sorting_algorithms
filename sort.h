c3915f09c3b6adf20c1b610e67ed2b9c944a28fb
brayohmurithidev <murithibrianm@gmail.com>
2022-10-11 23:54:49 +0300

    Sorting algorithms

--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef SORT_H_
+#define SORT_H_
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,22 +16,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
+/* print functions */
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* sorting functions */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* quick sort process function portotype */
-
-int Lomuto_partition(int *array, int low, int high, size_t size);
-void swap(int *array, int *i, int *j, size_t size);
-void quicksort_recurssion(int *array, int low, int high, size_t size);
-
-/* merge sorting process function prototype */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void merge_sort_recurrsion(int *array, int *temp, int left, int right);
-void merge_sort_array(int *array, int *temp, int left, int middle, int right);
-
 #endif
